1bab4325726813764da51c451ce5164be6f0d03f
aicayzer <aicayzer@icloud.com>
2020-02-19 07:48:28 +0000

    adding online resourses to the file

---
+++ Udemy/second_component/index.js
@@ -0,0 +1,13 @@
+class JSXDemo extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Hello there!</h1>
+				<h1>Hello there!!</h1>
+				<h1>Hello there!</h1>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<JSXDemo />, document.getElementById('root'));
